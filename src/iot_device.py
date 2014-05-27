4a32242a217049438bdab7d0a767fd8ac6b13a84
systmkor <systmkor@gmail.com>
2014-05-27 12:02:27 -0700

    CLI Option Parsing Started The option parsing for this program is started but not complete  On branch master  Changes to be committed: 	modified:   src/iot_device.py

--- src/iot_device.py
+++ src/iot_device.py
@@ -14,6 +14,10 @@
 import logging
 from optparse import OptionParser
 import sleekxmpp
+from sleekxmpp.plugins.xep_0325 import device as ControlDevice
+from sleekxmpp.plugins.xep_0323 import device as SensorDevice
+
+LOG_FORMAT = '%(levelname)-8s %(message)s'
 
 
 class IoTDevice(sleekxmpp.ClientXMPP):
@@ -26,24 +30,65 @@ class IoTDevice(sleekxmpp.ClientXMPP):
         sleekxmpp.ClientXMPP.__init__(self, jid, password)
         self.add_event_handler("session_start", self.session_start)
 
-
     def session_start(self, event):
         self.send_presence()
         self.get_roster()
 
-class Controll(sleekxmpp.plugins.xep_0325.device):
-    def __init__():
-        pass
 
-class Sensor(sleekxmpp.plugins.xep_0323.device):
-    def __init__():
-        pass
+# class Control(ControlDevice):
+#     def __init__(self, nodeId):
+#         self.control_fields = {}
+#
+#
+# class Sensor(SensorDevice):
+#     def __init__(self, nodeId, fields):
+#         self.nodeId = nodeId
 
-if __name__ == '__main__':
+def generate_options():
     optp = OptionParser()
 
-    # option handling
+    optp.add_option('-q', '--quiet', help='set logging to ERROR',
+                    action='store_const', dest='loglevel',
+                    const=logging.ERROR, default=logging.INFO)
+    optp.add_option('-d', '--debug', help='set logging to DEBUG',
+                    action='store_const', dest='loglevel',
+                    const=logging.DEBUG, default=logging.INFO)
+    optp.add_option('-v', '--verbose', help='set logging to COMM',
+                    action='store_const', dest='loglevel',
+                    const=5, default=logging.INFO)
+    # optp.add_option('-t', '--pingto', help='set jid to ping',
+    #                 action='store', type='string', dest='pingjid',
+    #                 default=None)
 
+    # JID and password options.
+    optp.add_option("-j", "--jid", dest="jid",
+                    help="JID to use")
+    optp.add_option("-p", "--password", dest="password",
+                    help="password to use")
+
+    # IoT
+    optp.add_option("-n", "--nodeid", dest="nodeid",
+                    help="I am a device get ready to be called", default=None)
+    return optp
+
+def process_options(optp):
+
+    options, args = optp.parse_args()
+    if len(args) == 0:
+        optp.print_help()
+        optp.exit()
+
+    return tuple(options, args)
+
+
+if __name__ == '__main__':
+
+    optp = generate_options()
+    options, args = process_options(optp)
+
+    logging.basicConfig(level=options.loglevel, format=LOG_FORMAT)
+
+    xmpp = IoTDevice(options.jid, options.password)
     # register nescessary plugins
     xmpp.register_plugin('xep_0030')
     xmpp.register_plugin('xep_0325')
@@ -54,5 +99,3 @@ if __name__ == '__main__':
     # begin stuff
     xmpp.connect()
     xmpp.process(block=True)
-
-
