2333346ff331815f7a18e757cc5a89bcfd15c2e3
systmkor <systmkor@gmail.com>
2014-05-28 15:45:14 -0700

    Created skeleton of IoT Console This is meant to be a CLI console to interact with IoT Devices  On branch master  Changes to be committed: 	new file:   src/iot_console.py

---
+++ src/iot_console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    SleekXMPP: The Sleek XMPP Library
+    Implementation of xeps for Internet of Things
+    http://wiki.xmpp.org/web/Tech_pages/IoT_systems
+    Copyright (C) 2013 Sustainable Innovation, Joachim.lindborg@sust.se
+    This file is part of SleekXMPP.
+
+    See the file LICENSE for copying permission.
+"""
+
+import sleekxmpp
+
+
+class IoTConsole(sleekxmpp.ClientXMPP):
+    pass
+
+
+if __name__ == '__main__':
+    pass
