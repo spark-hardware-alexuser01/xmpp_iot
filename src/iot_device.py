4cc51fbaeaee20cbb9a0d48429a90fd00790a1c7
systmkor <systmkor@gmail.com>
2014-05-28 12:35:44 -0700

    Removed Deprecated Option Parser PROGRAM DOES NOT WORK  On branch master  Changes to be committed: 	modified:   src/iot_device.py

--- src/iot_device.py
+++ src/iot_device.py
@@ -12,10 +12,9 @@
 """
 
 import logging
-from optparse import OptionParser
 import sleekxmpp
-from sleekxmpp.plugins.xep_0325 import device as ControlDevice
-from sleekxmpp.plugins.xep_0323 import device as SensorDevice
+from controls import Controls
+from sensors import Sensors
 
 LOG_FORMAT = '%(levelname)-8s %(message)s'
 
@@ -44,47 +43,10 @@ class IoTDevice(sleekxmpp.ClientXMPP):
 #     def __init__(self, nodeId, fields):
 #         self.nodeId = nodeId
 
-def generate_options():
-    optp = OptionParser()
-
-    optp.add_option('-q', '--quiet', help='set logging to ERROR',
-                    action='store_const', dest='loglevel',
-                    const=logging.ERROR, default=logging.INFO)
-    optp.add_option('-d', '--debug', help='set logging to DEBUG',
-                    action='store_const', dest='loglevel',
-                    const=logging.DEBUG, default=logging.INFO)
-    optp.add_option('-v', '--verbose', help='set logging to COMM',
-                    action='store_const', dest='loglevel',
-                    const=5, default=logging.INFO)
-    # optp.add_option('-t', '--pingto', help='set jid to ping',
-    #                 action='store', type='string', dest='pingjid',
-    #                 default=None)
-
-    # JID and password options.
-    optp.add_option("-j", "--jid", dest="jid",
-                    help="JID to use")
-    optp.add_option("-p", "--password", dest="password",
-                    help="password to use")
-
-    # IoT
-    optp.add_option("-n", "--nodeid", dest="nodeid",
-                    help="I am a device get ready to be called", default=None)
-    return optp
-
-def process_options(optp):
-
-    options, args = optp.parse_args()
-    if len(args) == 0:
-        optp.print_help()
-        optp.exit()
-
-    return tuple(options, args)
 
 
 if __name__ == '__main__':
 
-    optp = generate_options()
-    options, args = process_options(optp)
 
     logging.basicConfig(level=options.loglevel, format=LOG_FORMAT)
 
@@ -95,6 +57,12 @@ if __name__ == '__main__':
     xmpp.register_plugin('xep_0323')
 
     # create sensors and controlls
+    sensors = Sensors('thermometer')
+    sensors._add_field(name="temperature", typename="numeric", unit="C")
+    xmpp['xep_0323'].register_node(nodeId='thermometer',
+                                   device=sensors,
+                                   commTimeout=10)
+    controls = Controls('switch')
 
     # begin stuff
     xmpp.connect()
