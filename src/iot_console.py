0110d1508dd3c43f01a3fc10db12dceb450d4fce
systmkor <systmkor@gmail.com>
2014-05-28 22:29:06 -0700

    Flushed out a basic IoT Console Sets all basics up and attempts to request Thermometer form IoT Device  On branch master  Changes to be committed: 	modified:   src/iot_console.py

--- src/iot_console.py
+++ src/iot_console.py
@@ -11,12 +11,50 @@
     See the file LICENSE for copying permission.
 """
 
+import logging
 import sleekxmpp
+
+import cli_argparse
+
+
+LOG_FORMAT = '%(levelname)-8s %(message)s'
+log = logging.getLogger(__name__)
 
 
 class IoTConsole(sleekxmpp.ClientXMPP):
-    pass
+    """
+    An IoT console to act as a user interface to send commands/requests
+    to IoT devices that have controls or sensors.
+    """
+
+    def __init__(self, jid, password):
+        sleekxmpp.ClientXMPP.__init__(self, jid, password)
+        self.add_event_handler("session_start", self.session_start)
+
+    def session_start(self, event):
+        self.send_presence()
+        self.get_roster()
+
+        xmpp['xep_0323'].request_data(from_jid=xmpp.boundjid.bare,
+                                      to_jid='node0000@xmpp.jp',
+                                      callback=sensor_readout,
+                                      nodeIds=['thermometer'])
+
+
+def sensor_readout(from_jid, result, nodeId, timestamp, fields, error_msg):
+    log.DEBUG('sensor_readout()')
 
 
 if __name__ == '__main__':
-    pass
+    arg_parser = cli_argparse.gen_args()
+    args = cli_argparse.parse_args(arg_parser)
+
+    logging.basicConfig(level=args.loglevel, format=LOG_FORMAT)
+
+    xmpp = IoTConsole(args.jid, args.password)
+    xmpp.register_plugin('xep_0030')
+    xmpp.register_plugin('xep_0325')
+    xmpp.register_plugin('xep_0323')
+
+    xmpp.connect()
+    xmpp.process(block=True)
