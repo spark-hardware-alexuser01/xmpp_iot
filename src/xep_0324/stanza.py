719f54a24506f9504b1884c0d4d2024047feda38
systmkor <systmkor@gmail.com>
2014-05-31 18:40:41 -0700

    Possibly Figured out Sub Stanza Credentials Issue Check xep_0325/control.py and xep_0325/stanza/control.py for guidence You add your sub stanzas (i.e. credentials) to the iterables of CanAccess. Then you register stanza plugins and then you add_credential in our not stanza file but our provisioning.py (i.e. to be file). Where we will eventuall do iq['get'].add_credential('ip4', '10.0.0.1')  On branch master  Changes to be committed: 	modified:   xep_0324/stanza.py

--- src/xep_0324/stanza.py
+++ src/xep_0324/stanza.py
@@ -1,8 +1,9 @@
-from sleekxmpp.xmlstream import ET, ElementBase, register_stanza_plugin  
+from sleekxmpp.xmlstream import ET, ElementBase, register_stanza_plugin
+from sleekxmpp.stanza import Iq
 
-class Provisioning(ElementBase): 
+class Provisioning(ElementBase):
    """
-   A stanza class for IoT Provisioning 
+   A stanza class for IoT Provisioning
 
    example
    <iq type = 'get'
@@ -23,27 +24,27 @@ class FriendRequest(ElementBase):
    name = 'isFriend'
    namespace = 'urn:xmpp:iot:provisioning'
    interfaces = set(['jid'])
-   plugin_attrib = name 
+   plugin_attrib = name
 
 class Unfriend(ElementBase):
    name = 'unfriend'
    namespace = 'urn:xmpp:iot:provisioning'
    interfaces = set(['jid'])
-   plugin_attrib = name 
+   plugin_attrib = name
 
 class RecommendFriend(ElementBase):
    name = 'friend'
    namespace = 'urn:xmpp:iot:provisioning'
    interfaces = set(['jid'])
-   plugin_attrib = name 
+   plugin_attrib = name
 
 """I did not do rejecting read-outs because it was an
    extension of sensor data, however I studied it for
-   a while, and realized I have no idea how to do the 
+   a while, and realized I have no idea how to do the
    nodes part, saw some examples of how it might
    be done through sensor data"""
 
-class GetToken(ElementBase): 
+class GetToken(ElementBase):
    name = 'getToken'
    namespace = 'urn:xmpp:iot:provisioning'
    interfaces = set(tuple())
@@ -69,11 +70,63 @@ class CanControl(ElementBase):
    interfaces = set(['jid', 'serviceToken', 'userToken'])
    plugin_attrib = name
 
-class ClearCache(ElementBase): 
+class ClearCache(ElementBase):
    name = 'clearCache'
    namespace = 'urn:xmpp:iot:provisioning'
    interfaces = set(tuple())
    plugin_attrib = name
+
+
+
+
+class CanAccess(ElementBase):
+    name = 'canAccess'
+    namespace = 'urn:xmpp:iot:provisioning'
+    plugin_attrib = name
+    interfaces = set(['credentials', 'serviceToken'])
+
+    def __init__(self, xml=None, parent=None):
+        ElementBase.__init__(self, xml, parent)
+        self._credentials = set()
+
+    def setup(self, xml=None):
+        ElementBase.setup(self, xml)
+        self._credentials = set([credential['type'] for credential in self['credentials']])
+
+    def add_credential(self, name, typename, value):
+        """
+        Add a new credential to CanAccess request.
+
+        Arguments:
+            name  -- The name of the credential
+            typename -- the credential type
+            value -- the value for the credential
+        """
+
+        if name not in self._credentials:
+            credential = Credentials()
+
+            credential['name'] = name
+            credential['value'] = value
+
+            self._credentials.add(name)
+            self.iterables.append(credential)
+            return credential
+
+        return None
+
+    def get_credentials(self):
+        credentials = []
+        for credential in self['substanzas']:
+            if isinstance(credential, Credentials):
+                credentials.append(credential)
+        return credentials
+
+
+class Credentials(ElementBase):
+    name = 'credentials'
+    namespace = 'urn:xmpp:iot:provisioning'
+    interfaces = set(['name', 'value'])
 
 class CredentialParameter(ElementBase):
    """Parameter elements that act as credentials.
@@ -92,7 +145,7 @@ class CredentialParameter(ElementBase):
    namespace = 'urn:xmpp:iot:provisioning'
    name = 'credentialParameter'
    plugin_attrib = name
-   interfaces = set(['type', 'value'])
+   interfaces = set(['name', 'value'])
 
 class JidCredentialParameter(ElementBase):
    name = 'jid'
@@ -138,3 +191,6 @@ class ProtocolCredentialParameter(ElementBase):
    name = 'protocol'
    plugin_attrib = name
 
+
+register_stanza_plugin(Iq, CanAccess)
+register_stanza_plugin(CanAccess, Credentials, iterable=True)
